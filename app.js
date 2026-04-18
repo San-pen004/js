4a691cf6d4609b64267148354585bb05d5dda243
San_pen004 <negodyajka83@gmail.com>
2026-04-18 22:24:58 +1100

    adding operators

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 let variable1=3;
 let variable2=8;
+
 count=variable1+variable2;
+
 console.log(count)
+
 let name1="Sasha";
 let name2="Pen";
+
 personality=name1+name2
+
 console.log(personality)
+
+let age=17;
+
+if(age<18){
+    alert("Приходи, когда исполнится 18 лет");
+}
+else if(age>18 && age<99){
+    alert("Приятного просмотра")
+}
+else{
+    alert("Вам в другое место");
+}
