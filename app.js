ab94ecbf5e486538f56991b70d6569dae68e583d
San_pen004 <negodyajka83@gmail.com>
2026-04-18 23:38:28 +1100

    fix styles

--- app.js
+++ app.js
@@ -1,48 +1,42 @@
-let variable1=3;
-let variable2=8;
+let variable1 = 3;
+let variable2 = 8;
 
-count=variable1+variable2;
+count = variable1 + variable2;
 
-console.log(count)
+console.log(count);
 
-let name1="Sasha";
-let name2="Pen";
+let name1 = "Sasha";
+let name2 = "Pen";
 
-personality=name1+name2;
+personality = name1 + name2;
 
-console.log(personality)
+console.log(personality);
 
-let age=17;
+let age = 17;
 
-if(age<18){
-    alert("Приходи, когда исполнится 18 лет");
-}
-else if(age>18 && age<99){
-    alert("Приятного просмотра");
-}
-else{
-    alert("Вам в другое место");
+if (age < 18) {
+  alert("Приходи, когда исполнится 18 лет");
+} else if (age > 18 && age < 99) {
+  alert("Приятного просмотра");
+} else {
+  alert("Вам в другое место");
 }
 
-let title="pflfxf";
+let title = "pflfxf";
 
-if(title===""){
-    console.log("Название задачи не указано");
+if (title === "") {
+  console.log("Название задачи не указано");
+} else {
+  console.log("Задача:", title);
 }
 
-else{
-    console.log("Задача:",title);
+let tasks = 10;
+
+if (tasks === 0) {
+  console.log("Список пуст");
+} else if (tasks <= 3) {
+  console.log("Немного задач");
+} else if (tasks > 3) {
+  console.log("Много задач");
 }
 
-let tasks=10;
-
-if(tasks===0){
-    console.log("Список пуст")}
-
-else if(tasks<=3){
-    console.log("Немного задач")
-}
-
-else if(tasks>3){
-    console.log("Много задач")
-}
